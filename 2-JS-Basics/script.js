b09051ee072a85c1dd759cff23f5c5d84294beb0
srilatha chappidi <srilatha chappidi>
2017-04-29 17:00:47 -0400

    variables examples

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,46 @@
-console.log('hello world');
+//lecture on variable
+/*
+var name='john';
+console.log(name);
+
+var lastName='smith';
+console.log(lastName);
+
+var age = 25;
+console.log(age);
+
+var height = 5.5;
+console.log(height);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+//lecture variables continued
+
+var name = 'John';
+var age = 26;
+
+console.log(name+age);
+console.log(age+name);
+console.log(name+name);
+console.log(age+age);
+
+var job,married;
+
+console.log(job);
+console.log(job+married);
+
+job='software engineer';
+married=false;
+
+console.log(name+age+job+married);
+
+job = 'driver';
+age = 'twenty seven';
+console.log(name+age+job+married);
+
+var lastName = prompt('what is the last name? ');
+console.log(lastName);
+
+alert(name+age+job+married);
