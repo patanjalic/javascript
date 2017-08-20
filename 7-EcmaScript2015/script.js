15a2bfa58c2445c134869033d05cf9885e7b261f
patanjalic <patanjali2107@gmail.com>
2017-08-20 17:34:01 -0400

    let and const key words

---
+++ 7-EcmaScript2015/script.js
@@ -0,0 +1,22 @@
+
+function testScope5() {
+    console.log(name);
+    if(true) {
+        var name = 'john smith';
+        var age = 20;
+    }
+    console.log(name+' '+age);
+}
+
+testScope5();
+
+function testScope6() {
+    if(true) {
+        let name = 'john smith';
+        const age = 20;
+    }
+    console.log(name+' '+age);
+}
+
+testScope6();
+
