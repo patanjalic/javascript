f12eac021ceca412d3ac8e0782958e83b6adb531
patanjalic <patanjali2107@gmail.com>
2017-08-22 19:53:56 -0400

    destructuring

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -43,7 +43,7 @@ var ages1 = years.map((el,index) => {
 });
 
 console.log(ages1);
-*/
+
 
 
 var box5 = {
@@ -71,3 +71,53 @@ var box6 = {
     }
 };
 box6.clickMe();
+
+
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.myfriends = function(friends) {
+    return friends.map(function(el){
+        return this.name+' friends with '+el;
+    }.bind(this));
+}
+
+var john = new Person('John');
+var friends = ['bob','jane'];
+console.log(john.myfriends(friends));
+
+Person.prototype.myfriends6 = function(friends) {
+    return friends.map((el) => {
+        return `${this.name} friends6 with ${el}`;
+    });
+}
+
+var john = new Person('bob');
+var friends6 = ['bob','jane'];
+console.log(john.myfriends6(friends6));
+
+*/
+
+//desctructuring
+
+const [name,age] = ['john',26];
+
+console.log(name);
+console.log(age);
+
+const obj = {
+    'firstName':'john',
+    'age1':26
+};
+
+const {firstName,age1} = obj;
+console.log(firstName);
+console.log(age1);
+
+function calculateRetirement(age) {
+    return [age,65-age];
+}
+
+const [age2,retirement] = calculateRetirement(26);
+console.log(age2+' '+retirement);
